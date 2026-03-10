c33d219818927fda0f5fc7fa9657af01976cb983
Lexie Choi <lexiechoi852@gmail.com>
2026-03-10 01:40:13 -0400

    feat: add database connection

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from sqlmodel import Session, select
+from app.database import engine
 
 app = FastAPI()
 
@@ -6,3 +8,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/db-test")
+def test_db():
+    try:
+        with Session(engine) as session:
+            session.exec(select(1)).first()  # simple query
+        return {"status": "database connected!"}
+    except Exception as e:
+        return {"status": "error", "detail": str(e)}
